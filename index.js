f8b731d7f35b39e4bf51bd558d8e732a7043d03b
ko <ko>
2019-09-10 16:58:31 +0900

    use middleware

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
-const app = express(); //application을 만듬
-// () => { } 함수
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+const app = express();
 
 const PORT = 4000;
 
@@ -10,7 +13,15 @@ const handleHome = (req, res) => res.send("Hello from ass");
 
 const handleProfile = (req, res) => res.send("You are on my profile"); //babel 의 함수사용방법  =>  <- arrow function
 
+
+app.use(cookieParser());
+app.use(bodyParser.json({extends : true}));
+app.use(bodyParser.urlencoded({extends : true}));
+app.use(helmet());
+app.use(morgan("dev"));
+
 app.get("/", handleHome);
+
 app.get("/profile" , handleProfile);
 
 app.listen(PORT , handleListening);

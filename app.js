4bc1220d62f16a9ded7528a542317d2085dbf1f0
ko <ko>
2019-09-15 22:14:02 +0900

    MVC

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 const app = express();
 
-const handleHome = (req, res) => res.send("Hello from ass");
-
-const handleProfile = (req, res) => res.send("You are on my profile"); //babel 의 함수사용방법  =>  <- arrow function
 
 
 app.use(cookieParser());
@@ -17,10 +16,8 @@ app.use(bodyParser.urlencoded({extends : true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
 
-app.get("/profile" , handleProfile);
-
+app.use("/", globalRouter);
 app.use("/user", userRouter); //use의 의미는 누군가 /user경로에 접속하면 이 router전체를 사용하겠다는 의미
-
+app.use("/video", videoRouter);
 export default app; //누군가 import할때  app object를 주겠단 의미

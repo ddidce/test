c4b98a71560bc62c1f9502ed7f477f0fe043d819
ko <ko>
2019-09-17 00:31:36 +0900

    MVC2

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 const app = express();
 
 
@@ -17,7 +18,9 @@ app.use(helmet());
 app.use(morgan("dev"));
 
 
-app.use("/", globalRouter);
-app.use("/user", userRouter); //use의 의미는 누군가 /user경로에 접속하면 이 router전체를 사용하겠다는 의미
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter); //use의 의미는 누군가 /user경로에 접속하면 이 router전체를 사용하겠다는 의미
+app.use(routes.videos, videoRouter);
+
+
 export default app; //누군가 import할때  app object를 주겠단 의미
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,5 +2,6 @@ import express from "express";
 
 const userRouter = express.Router();
 
+
 export default userRouter;
 

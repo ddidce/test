d1f8cdb72b7c78e60f46ccd968fc8db17e4e1697
ko <ko>
2019-09-17 19:10:19 +0900

    study pug

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,7 @@
-export const home = (req, res) => res.send("Home");
-export const search = (req, res) => res.send("Search");
-export const videos = (req, res) => res.send("Videos");
-export const upload = (req, res) => res.send("Upload");
-export const videoDetail = (req, res) => res.send("Video Detail");
-export const editVideo = (req, res) => res.send("Edit Video");
-export const deleteVideo= (req, res) => res.send("Delete Video");
+export const home = (req, res) => res.render("home"); //send대신 render사용시 views폴더에서 하일명이 home이고 확장자가pug인 템플릿파일을 찾음
+export const search = (req, res) => res.render("Search");
+export const videos = (req, res) => res.render("Videos");
+export const upload = (req, res) => res.render("Upload");
+export const videoDetail = (req, res) => res.render("Video Detail");
+export const editVideo = (req, res) => res.render("Edit Video");
+export const deleteVideo= (req, res) => res.render("Delete Video");

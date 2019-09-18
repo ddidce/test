fa2ced069d077e5d35eab9d7c0186f9f31a25b76
ko <ko>
2019-09-18 23:58:26 +0900

    contorller

--- app.js
+++ app.js
@@ -3,19 +3,23 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddelware } from "./middlewares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
 const app = express();
 
 app.set('view engine', "pug"); //view engine은 처음에 default라서 pug엔진으로 바꿔줌
 
+app.use(helmet()); //helmet은 application을 더 안전하도록 도와줌
 app.use(cookieParser()); //cookieParser은 쿠키를 전달받아서 사용할 수 있도록 하는 미들웨어
 app.use(bodyParser.json({extends : true})); //bodyParser은 사용자가 웹사이트로 전달하는 정보들을 검사하는 미들웨어
 app.use(bodyParser.urlencoded({extends : true}));
-app.use(helmet()); //helmet은 application을 더 안전하도록 도와줌
 app.use(morgan("dev")); //morgan은 application에서 발생하는 일들을 loging하는 것
+app.use(localsMiddelware);
+
+
 
 app.use(routes.home, globalRouter); //이안에 홈, 서치, 조인, 로그인, 로그아웃URL이 담겨있음
 app.use(routes.users, userRouter); //use의 의미는 누군가 /user경로에 접속하면 이 router전체를 사용하겠다는 의미 , user URL이 담겨있음 주소는 routes에 정의
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,20 @@
-export const home = (req, res) => res.render("home"); //send대신 render사용시 views폴더에서 하일명이 home이고 확장자가pug인 템플릿파일을 찾음
-export const search = (req, res) => res.render("Search");
-export const videos = (req, res) => res.render("Videos");
-export const upload = (req, res) => res.render("Upload");
-export const videoDetail = (req, res) => res.render("Video Detail");
-export const editVideo = (req, res) => res.render("Edit Video");
-export const deleteVideo= (req, res) => res.render("Delete Video");
+export const home = (req, res) => res.render("home", {pageTitle: "Home"}); //send대신 render사용시 views폴더에서 하일명이 home이고 확장자가pug인 템플릿파일을 찾음
+export const search = (req, res) => {
+    console.log(req);
+    res.render("search", {pageTitle: "Search"});
+
+}
+export const videos = (req, res) =>
+ res.render("videos", {pageTitle: "Videos"});
+
+export const upload = (req, res) =>
+ res.render("upload", {pageTitle: "Upload"});
+
+export const videoDetail = (req, res) => 
+res.render("video Detail", {pageTitle: "Video Detail"});
+
+export const editVideo = (req, res) => 
+res.render("edit Video", {pageTitle: "Edit Video"});
+
+export const deleteVideo= (req, res) => 
+res.render("delete Video", {pageTitle: "Delete Video"});

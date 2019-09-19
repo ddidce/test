e11050e21229f6c542b323fe078125d0d0475421
ko <ko>
2019-09-20 02:43:28 +0900

    Home Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,11 +1,16 @@
-export const home = (req, res) => res.render("home", {pageTitle: "Home"}); //send대신 render사용시 views폴더에서 하일명이 home이고 확장자가pug인 템플릿파일을 찾음
+import { videos } from "../db";
+
+export const home = (req, res) => {
+    res.render("home", { pageTitle: "Home", videos });
+  }; //send대신 render사용시 views폴더에서 하일명이 home이고 확장자가pug인 템플릿파일을 찾음
+
 export const search = (req, res) => {
     const {query : {term : searchingBy}} = req; //query : {term} = req.query.term
     res.render("search", {pageTitle: "Search", searchingBy});
 
 }
-export const videos = (req, res) =>
- res.render("videos", {pageTitle: "Videos"});
+// export const videos = (req, res) =>
+//  res.render("videos", {pageTitle: "Videos"});
 
 export const upload = (req, res) =>
  res.render("upload", {pageTitle: "Upload"});
---
+++ db.js
@@ -0,0 +1,53 @@
+export const videos = [
+    {
+        id: 324393,
+        title: "Video awesome",
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "KYS",
+            email: "kys213@gaver.com",
+        }
+    },
+
+    {
+        id: 123125,
+        title: "Video super",
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "KYS",
+            email: "kys213@gaver.com", 
+        }
+    },
+
+    {
+        id: 315132,
+        title: "Video nice",
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "KYS",
+            email: "kys213@gaver.com",
+        }
+    },
+
+    {
+        id: 125132,
+        title: "Video perfect",
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "KYS",
+            email: "kys213@gaver.com",
+        }
+    }
+];

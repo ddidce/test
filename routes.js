49a28492a77c0ec58229c847c342056cdcbadb89
ko <ko>
2019-09-23 23:36:37 +0900

    Home Controller part2

--- routes.js
+++ routes.js
@@ -18,7 +18,7 @@ const CHANGE_PASSWORD = "/change-password";
 const VIDEOS = "/videos";
 const UPLOAD = "/upload";
 const VIDEO_DETAIL = "/:id";
-const EDIT_VIDEO = ":id/edit";
+const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
 
 const routes = {

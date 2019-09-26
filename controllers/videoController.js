0f07dedb0f6fc69e693967db28e42da0710a8bbc
ko <ko>
2019-09-27 00:53:58 +0900

    Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,19 +1,33 @@
 import { videos } from "../db";
+import routes from "../routes";
 
 export const home = (req, res) => {
     res.render("home", { pageTitle: "Home", videos });
   }; //send대신 render사용시 views폴더에서 하일명이 home이고 확장자가pug인 템플릿파일을 찾음
 
 export const search = (req, res) => {
-    const {query : {term : searchingBy}} = req; //query : {term} = req.query.term
-    res.render("search", {pageTitle: "Search", searchingBy});
+    const {
+      query : {term : searchingBy}
+    } = req; //query : {term} = req.query.term
+    res.render("search", {pageTitle: "Search", searchingBy, videos});
 
 }
 // export const videos = (req, res) =>
 //  res.render("videos", {pageTitle: "Videos"});
 
-export const upload = (req, res) =>
+export const getUpload = (req, res) =>
  res.render("upload", {pageTitle: "Upload"});
+export const postUpload = (req, res) => {
+  const {
+    body: {
+      file,
+      title,
+      description
+    }
+  } = req;
+  //To do : video Upload and save
+  res.redirect(routes.videoDetail(324393));
+};
 
 export const videoDetail = (req, res) => 
 res.render("videoDetail", {pageTitle: "Video Detail"});
--- middlewares.js
+++ middlewares.js
@@ -2,6 +2,10 @@ import routes from "./routes";
 
 export const localsMiddelware = (req, res, next) => {
     res.locals.siteName = "Youtube";
-    res.locals.routes = routes
+    res.locals.routes = routes;
+    res.locals.user= {
+        isAuthenticated: true,
+        id: 1
+    };
     next();
-}
+};
